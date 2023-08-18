09222d573f651e0ebc8abc92a291e2c280168cdb
Juan Carlos Gómez Correa <juancarlos.correa@externos-cl.cencosud.com>
2023-08-18 12:08:40 -0400

    wip

--- cloud_component/scripts/pre.js
+++ cloud_component/scripts/pre.js
@@ -1,18 +1,24 @@
 module.exports = async (runner, args) => {
   try {
-    console.log('> PRE: Start');
+    console.log('> PRE: Start')
 
-    // const rc = args.rc;
-    // await runner.execute([
-    //   `npx nx g @nrwl/web:app ${rc.path}`
-    // ], {
-    //   cwd: rc.workspace_path
-    // })
+    console.log(`args=${JSON.stringify(args)}`);
+
+    const rc = args.rc;
+    await runner.execute([
+      `echo '---------------'`,
+      `ls -alh`,
+      `echo '---------------'`,
+      `ls -Ralh ${rc.path}`,
+      `echo '---------------'`
+    ], {
+      cwd: rc.workspace_path
+    })
 
     console.log('> PRE: ✅ DONE')
 
   } catch (ex) {
-    console.error(ex);
-    throw new Error('Failed to run script');
+    console.error(ex)
+    throw new Error('Failed to run script')
   }
 }
